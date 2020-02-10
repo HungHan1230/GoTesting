8d123be9fcbd660adf0651be8555c893c49564be
HungHan1230 <s0963360045@gmail.com0>
2020-02-10 17:35:52 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	TransposeMatrix "github.com/MyTestingGolangFuncs/TransposeMatrix"
+
+	MyTestingReadFile "github.com/MyTestingGolangFuncs/MyTestingReadFile"
+)
+
+func main() {
+	mainTransposeMatrix()
+	mainMyTestingReadFile()
+}
+
+func mainTransposeMatrix() {
+	sample := [][]string{
+		[]string{"a1", "a2", "a3", "a4", "a5"},
+		[]string{"b1", "b2", "b3", "b4", "b5"},
+		[]string{"c1", "c2", "c3", "c4", "c5"},
+	}
+	ar := TransposeMatrix.Transpose(sample)
+	fmt.Println(ar)
+}
+func mainMyTestingReadFile() {
+	MyTestingReadFile.Run()
+}
